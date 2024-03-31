15e82668f959b8afd2600609cb8c6fe6d9e99b28
aLLaN7654 <163740791+aLLaN7654@users.noreply.github.com>
2024-03-31 16:41:24 +0500

    aaaaaaaa

---
+++ Assets/Scripts/FIreballCaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FIreballCaster : MonoBehaviour
+{
+    public Fireball fireballPrefab;
+    public Transform fireballpointTransform;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, fireballpointTransform.position, fireballpointTransform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/zadacy 1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zadacy1 : MonoBehaviour
+{
+
+    public Transform point1;
+    public Transform point2;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(point1.position, point2.position, 0.5f);
+    }
+}
